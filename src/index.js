3f44e4ef9871c2dfa15aa289aaea6c104894ca79
Mihir Patel <mihirbhalani18@gmail.com>
2024-02-27 23:18:39 +0530

    secound commit for the app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import React from 'react';
-import ReactDom from 'react-dom/client'
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+import searchImages from "./api";
+
+searchImages();
+
+const el = document.getElementById("root");
+const root = ReactDom.createRoot(el);
+
+root.render(<App />);
