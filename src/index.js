1348e7b3f4699d01b11d3050d9f73aa08c49d3ef
Mihir Patel <mihirbhalani18@gmail.com>
2024-03-11 21:33:06 +0530

    initial code commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDom from "react-dom/client";
 import App from "./App";
 import searchImages from "./api";
 
-searchImages();
+// searchImages();
 
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
